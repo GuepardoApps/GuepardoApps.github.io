a65db6137693439548d280cf78373acec81e0433
Jonas Schubert <guepardoapps@gmail.com>
2019-04-19 09:53:26 +0200

    merge gh-pages into master

--- src/components/Header/Header.data.js
+++
@@ -1,5 +0,0 @@
-export default {
-    "name": "Jonas Schubert",
-    "role": "Software Developer Web",
-    "roleDescription": "I like to use modern web technologies like Angular, React and Vue, combined with modern backend technologies like .NET Core. I am always interested in learning new technologies. Also in my free time I create open source applications."
-};
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import About from "./About/About";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Projects from "./Projects/Projects";
-import Resume from "./Resume/Resume";
-
-export default {
-    About, Footer, Header, Projects, Resume
-};
