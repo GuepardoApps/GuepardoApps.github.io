da2785c33cb6e405d3db8c0ccf8fa4ddb9b9dfc0
Jonas Schubert <guepardoapps@gmail.com>
2019-10-31 20:14:11 +0100

    Updates page

---
+++ precache-manifest.5d0fa0aaede178a52448a2f3f41a1f27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06bf6eb266d0b32202fc720e204716a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e3b7bf83ef47ba3597b",
+    "url": "./static/css/main.9d1d725f.chunk.css"
+  },
+  {
+    "revision": "4db0344d505aeb32e548",
+    "url": "./static/js/2.2f2af8a7.chunk.js"
+  },
+  {
+    "revision": "8e3b7bf83ef47ba3597b",
+    "url": "./static/js/main.55da7f30.chunk.js"
+  },
+  {
+    "revision": "d70a901aec3e881e772d",
+    "url": "./static/js/runtime-main.fc4394dc.js"
+  }
+]);
