86bfdbd2ea853347d3c34b1526a025aa73170a6b
Jonas Schubert <jonas.schubert.projects@web.de>
2019-11-09 13:03:50 +0100

    Updates

---
+++ precache-manifest.5c62ef9089aa98e6b98c4ce32b53c418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9a550fd57473416a0d5f32af9600fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74cf41ab1f560f2c7ccc",
+    "url": "./static/css/main.9d1d725f.chunk.css"
+  },
+  {
+    "revision": "da20a55b9fa776bd2f57",
+    "url": "./static/js/2.60ccb449.chunk.js"
+  },
+  {
+    "revision": "74cf41ab1f560f2c7ccc",
+    "url": "./static/js/main.fb266148.chunk.js"
+  },
+  {
+    "revision": "55563b0c0f5e3d748547",
+    "url": "./static/js/runtime-main.ba7b5fd5.js"
+  }
+]);
