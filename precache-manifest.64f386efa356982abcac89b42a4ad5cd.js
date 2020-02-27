58381965fc18f525cc10e59ce117d7869ac32914
Jonas Schubert <jonas.schubert.projects@web.de>
2020-02-27 21:55:46 +0100

    new release, v2.0.9

---
+++ precache-manifest.64f386efa356982abcac89b42a4ad5cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9285d5e8cb8563a4178d5a6a8b3211aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc92457bed747a7f00dd",
+    "url": "./static/css/main.77cc475c.chunk.css"
+  },
+  {
+    "revision": "01ef5b4992ca277d916d",
+    "url": "./static/js/2.7a2c10a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.7a2c10a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc92457bed747a7f00dd",
+    "url": "./static/js/main.9c92c01f.chunk.js"
+  },
+  {
+    "revision": "ace0176d682177c43e4e",
+    "url": "./static/js/runtime-main.954c299c.js"
+  }
+]);
