46bfb323b86d50dc15c5f410101bb347c6b945e9
Jonas Schubert <jonas.schubert.projects@web.de>
2020-03-11 22:38:05 +0100

    data update

--- src/components/About/About.data.js
+++ src/components/About/About.data.js
@@ -1,6 +1,6 @@
 export default {
     "name": "Jonas Schubert",
-    "text": "I studied Medical engineering and finished my M.Sc. in 2016 with the grade 1.6. During my studies I taught myself software developing. I started my work life in the field of medical devices, switched to car testing and now I am a web developer at <a className=\"about-link\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://www.siemens.com\">Siemens AG</a>. My passions are open source, mobile & web and coding for raspberry pi.",
+    "text": "I studied Medical engineering and finished my M.Sc. in 2016 with the grade 1.6. During my studies I taught myself software developing. I started my work life in the field of medical devices, switched to car testing and now I am a web developer at <a className=\"about-link\" target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://www.siemens.com\">Siemens AG</a> using VueJs. My passions are open source, mobile & web and coding for raspberry pi.",
     "contacts": [
         {
             "icon": "fas fa-map-marker",
--- src/components/Header/Header.data.js
+++ src/components/Header/Header.data.js
@@ -1,5 +1,5 @@
 export default {
     "name": "Jonas Schubert",
     "role": "Software Developer for Web",
-    "roleDescription": "I like to use modern web technologies like Vue, Angular and React, combined with modern backend technologies like .NET Core. I am always interested in learning new technologies. In my free time I participate in open source applications."
+    "roleDescription": "I like to use modern web technologies like VueJs, Angular and React, combined with modern backend technologies like .NET Core. I am always interested in learning new technologies. In my free time I participate in open source applications."
 };
